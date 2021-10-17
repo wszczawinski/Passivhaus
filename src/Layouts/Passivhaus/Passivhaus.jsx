a7ff531a2ea80befc732a120cc2f8ab25cb5e5db
kingazawarczynska <zawarczynska.ko@gmail.com>
2021-10-17 13:20:27 +0200

    feat(routing): export routing components and create its index

---
+++ src/Layouts/Passivhaus/Passivhaus.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Passivhaus() {
+	return <div>Passivhaus</div>;
+}
---
+++ src/Layouts/Workshops/Workshops.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Workshops() {
+	return <div style={{height: "100vh"}}>Sszkolenia</div>;
+}
