fc7e4da877e8d6e9fe27d5773c10d2305e88c797
kingazawarczynska <zawarczynska.ko@gmail.com>
2021-11-11 20:38:40 +0100

    feat(desktop-nav): style nav in desktop view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,61 +8,66 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import pibpLogo from '../../images/pibpLogo.png';
 
 export function Navbar() {
-    const [navVisibility, setNavVisibility] = useState(false);
+	const [ navVisibility, setNavVisibility ] = useState(false);
+	const mobileView = !window.matchMedia('(min-width: 1000px)');
 
-    let changeNavVisibility = () => {
-        setNavVisibility(!navVisibility);
-    };
+	let changeNavVisibility = () => {
+		if (mobileView) {
+			setNavVisibility(!navVisibility);
+		}
+	};
 
-    return (
-        <nav className="navbar">
-            <img className="navbar__logo" src={pibpLogo} alt="PiBP Logo" />
+	return (
+		<nav className="navbar">
+			<img className="navbar__logo" src={pibpLogo} alt="PiBP Logo" />
 
-            <ul className={`navbar__list ${navVisibility ? 'navbar__visible' : ''}`}>
-                <li>
-                    <Link to="/" onClick={changeNavVisibility}>
-                        Firma
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/budynek-pasywny" onClick={changeNavVisibility}>
-                        Budynek pasywny
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/certyfikacja" onClick={changeNavVisibility}>
-                        Certyfikacja
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/szkolenia" onClick={changeNavVisibility}>
-                        Szkolenia
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/literatura+narzedzia" onClick={changeNavVisibility}>
-                        Literatura i narzędzia
-                    </Link>
-                </li>
-                <li>
-                    <Link to="/ambasador" onClick={changeNavVisibility}>
-                        Ambasador Budownictwa Pasywnego
-                    </Link>
-                </li>
-                <li>
-                    <Button
-                        className={'btn__mini btn__white'}
-                        textContent={<FontAwesomeIcon icon={faTimes} />}
-                        onClick={changeNavVisibility}
-                    />
-                </li>
-            </ul>
+			<ul className={`navbar__list ${navVisibility ? 'navbar__visible' : ''}`}>
+				<div className="navbar__list--upper-items">
+					<li>
+						<Link to="/" onClick={changeNavVisibility}>
+							Firma
+						</Link>
+					</li>
+					<li>
+						<Link to="/budynek-pasywny" onClick={changeNavVisibility}>
+							Budynek pasywny
+						</Link>
+					</li>
+					<li>
+						<Link to="/certyfikacja" onClick={changeNavVisibility}>
+							Certyfikacja
+						</Link>
+					</li>
+					<li>
+						<Link to="/szkolenia" onClick={changeNavVisibility}>
+							Szkolenia
+						</Link>
+					</li>
+					<li>
+						<Link to="/literatura+narzedzia" onClick={changeNavVisibility}>
+							Literatura i narzędzia
+						</Link>
+					</li>
+				</div>
 
-            <Button
-                className={'btn__mini btn__black'}
-                textContent={<FontAwesomeIcon icon={faBars} />}
-                onClick={changeNavVisibility}
-            />
-        </nav>
-    );
+				<li className="navbar__list--lower-item">
+					<Link to="/ambasador" onClick={changeNavVisibility}>
+						Ambasador Budownictwa Pasywnego
+					</Link>
+				</li>
+				<li>
+					<Button
+						className={'btn__mini btn__white navbar__hamburger'}
+						textContent={<FontAwesomeIcon icon={faTimes} />}
+						onClick={changeNavVisibility}
+					/>
+				</li>
+			</ul>
+			<Button
+				className={'btn__mini btn__black navbar__hamburger'}
+				textContent={<FontAwesomeIcon icon={faBars} />}
+				onClick={changeNavVisibility}
+			/>
+		</nav>
+	);
 }
