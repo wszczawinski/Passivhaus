6861c53a6ab232d6cafcb54462a19596b27aceb1
kingazawarczynska <zawarczynska.ko@gmail.com>
2021-11-12 13:49:05 +0100

    feat(hero+mobile-nav): add hero title and subtitle, adjust mobile nav

---
+++ src/heroContent.js
@@ -0,0 +1,8 @@
+export const heroText = {
+    firmText: <p>ALE PRZYSZŁOŚĆ <br /> TWORZYMY <br/> <u>AKTYWNIE</u></p>,
+    passivhausText: <p>BUDYNEK PASYWNY</p>,
+    certificationText: <p>CERTYFIKACJA</p>,
+    workshopsText: <p>SZKOLENIA</p>,
+    literatureText: <p>MATERIAŁY ONLINE</p>,
+    ambassadorText: <p>ZOSTAŃ <br/> AMBASADOREM!</p>
+}
