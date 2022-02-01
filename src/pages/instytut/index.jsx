a28643d767370fabf2a9d1c7738f350fb34c6d8e
Kinga Zawarczynska <kinga.zawarczynska@justjoin.it>
2022-02-01 19:15:02 +0100

    feat(nav): fix nav redirects, add components

--- src/pages/instytut/index.jsx
+++ src/pages/instytut/index.jsx
@@ -1,27 +1,144 @@
 import React from 'react';
-import { Button, Layout } from '../../components';
-import heroFirmImage from '../../images/heroImages/hero_firma.png';
-import { heroText } from '../../constants/heroContent';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Layout } from '../../components';
+import ContainerRow from '../../components/ContainerWithImage/ContainerRow';
+import oNas1 from "../../images/firm/o-nas-1.svg"
 
 import './Firm.scss';
 
-export default function Instytut() {
+export default function About() {
     return (
-        <Layout
-            heroTextContent={heroText.firmText}
-            heroBackgroundImage={heroFirmImage}
-            heroClass={'hero__subpage'}
-        >
+        <Layout>
             <section className="layout-content">
-                <h2>Firma</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, nihil est
-                    repellat consequatur quam enim! Error asperiores doloribus voluptatum eos
-                    consequuntur id animi nostrum. Vel consectetur explicabo officia atque natus!
-                </p>
+                <ContainerRow text={<p>
+                        Od 2004 roku jesteśmy niezależną polską instytucją, aktualnie działającą na
+                        zasadach non-profit. Naszym celem jest upowszechnianie wiedzy o{' '}
+                        <strong>
+                            budownictwie pasywnym, zero energetycznym, plus energetycznym oraz o
+                            kompleksowej termomodernizacji{' '}
+                        </strong>{' '}
+                        z wykorzystaniem technologii i komponentów pasywnych. Ponadto transferujemy
+                        wiedzę na temat energii odnawialnej, która z powodzeniem pokrywa potrzeby
+                        budynków energetycznie pasywnych.
+                    </p>} imgSrc={oNas1} alt="Budynek Instytutu w Gdańsku" />
 
-                <Button textContent={'Text'} />
+                <div className="about-paragraph__orange">
+                    <span className="about-paragraph__text">
+                        <p>
+                            Jesteśmy jedyną partnerską instytucją Instytutu Budownictwa Pasywnego z
+                            Darmstadt (PHI) i jako jedyni w Polsce podsiadamy akredytację PHI do
+                            przeprowadzania certyfikacji budynków pasywnych na terenie Europy.
+                        </p>
+                        <p>
+                            Na zdjęciach: Budynek biurowy Polskiego Instytutu Budownictwa Pasywnego
+                            i Energii Odnawialnej w Gdańsku oraz kompleks 3 domów (energooszczędny
+                            5l, niskoenergetyczny 3l, pasywny 1,5l).
+                        </p>
+                        <StaticImage src="../../images/firm/o-nas-2.svg" />
+                        <span className="about-paragraph__border--short"></span>
+                        <p>
+                            Naszym celem jest możliwie szeroka popularyzacja wiedzy na temat
+                            budownictwa pasywnego. Pragniemy stale poszerzać krąg sympatyków idei
+                            budownictwa pasywnego. Jest to właściwa droga do podniesienia, jakości
+                            życia oraz ochrony środowiska naturalnego, a przede wszystkim wyraźna
+                            pomoc w uzyskaniu niezależności energetycznej naszego kraju.
+                        </p>
+                        <p>
+                            Będziemy współpracować ze wszystkimi, którzy tak jak my pragną
+                            ograniczenia bezsensownej konsumpcji energetycznej a przy okazji celują
+                            w postęp z wykorzystaniem innowacyjnych rozwiązań w zakresie
+                            zrównoważonego energetycznie rozwoju.
+                        </p>
+                    </span>
+                </div>
+
+                <div className="about-paragraph__icons">
+                    <div className="about-paragraph__icons--row">
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/1.svg" />
+                            <p>ORGANIZACJA WYKŁADÓW, KONFERENCJI I SEMINARIÓW</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/2.svg" />
+                            <p>ORGANIZACJA SZKOLEŃ I KURSÓW DLA UCZESTNIKÓW PROCESU BUDOWLANEGO</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/3.svg" />
+                            <p>PROJEKTOWANIE BUDYNKÓW PASYWNYCH</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/4.svg" />
+                            <p>WERYFIKACJA PROJEKTÓW BUDOWLANYCH</p>
+                        </span>
+                    </div>
+                    <div className="about-paragraph__icons--row">
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/5.svg" />
+                            <p>CERTYFIKACJA BUDYNKÓW PASYWNYCH</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/6.svg" />
+                            <p>
+                            DYSTRYBUCJA NARZĘDZI (OPROGRAMOWANIE PHPP) WSPOMAGAJĄCYCH
+                                PROJEKTOWANIE NOWYCH BUDYNKÓW ORAZ TERMOMODERNIZACJĘ BUDYNKÓW
+                                ISTNIEJĄCYCH
+                            </p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/7.svg" />
+                            <p>
+                            DYSTRYBUCJA KSIĄŻEK (BUDOWNICTWO PASYWNE, CIEPŁOWNICTWO,
+                                KLIMATYZACJA)
+                            </p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/8.svg" />
+                            <p>
+                            ZWIEDZANIE BUDYNKÓW PASYWNYCH Z WYKŁADEM I KOMENTARZEM NA TERENIE
+                                POLSKI I NIEMIEC
+                            </p>
+                        </span>
+                    </div>
+                    <div className="about-paragraph__icons--row">
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/9.svg" />
+                            <p>
+                            UDZIELANIE PATRONATÓW MERYTORYCZNYCH ORAZ HONOROWYCH NA POTRZEBY
+                                POPULARYZACJI WYDARZEŃ TEMATYCZNYCH
+                            </p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/10.svg" />
+                            <p>WYPOŻYCZANIE WYSTAWY „BUDOWNICTWO PASYWNE OD A DO Z”</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/11.svg" />
+                            <p>WSPÓŁPRACA Z PORTALAMI BRANŻOWYMI I PRASĄ</p>
+                        </span>
+                        <span className="about-paragraph__icons--single-icon">
+                            <StaticImage src="../../images/firm/icons/12.svg" />
+                            <p>PROMOCJA FIRM POPULARYZUJĄCYCH BUDOWNICTWO PASYWNE</p>
+                        </span>
+                    </div>
+                </div>
+                <div className="about-paragraph__blue">
+                <span className="about-paragraph__border"></span>
+                    <p>
+                        Naszym głównym celem jest popularyzacja i transfer wiedzy z zakresu
+                        budownictwa pasywnego oraz szerokie promowanie tego standardu. Organizujemy
+                        konferencje, fora i spotkania dające możliwość wymiany doświadczeń,
+                        pogłębienia wiedzy oraz nawiązania nowych kontaktów. Przygotowujemy i
+                        opracowujemy materiały szkoleniowo - dydaktyczne (dla doskonalenia
+                        zawodowego) i informacyjne dla projektantów budynków pasywnych oraz
+                        wykonawców takich budynków. Chętnie wspieramy ambitne wydarzenia, takie jak
+                        międzynarodowe i lokalne targi budowlane, ponieważ poprawa efektywności
+                        energetycznej naszego kraju jest w największej mierze uzależniona od poziomu
+                        świadomości społeczeństwa polskiego.
+                        <StaticImage src="../../images/firm/icons/leaf.svg" />
+                    </p>
+                </div>
             </section>
         </Layout>
     );
 }
+
