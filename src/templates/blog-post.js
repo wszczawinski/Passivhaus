cb447ba380c35212fe9b0088bf3aa99a6b6e12f7
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2022-02-02 21:00:39 +0100

    feat(dynamicQueries): add dynamic queries to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import { Layout } from '../components';
-import { GatsbyImage } from 'gatsby-plugin-image';
 
-export default function BlogPost() {
+import { graphql } from 'gatsby';
+import { getImage, GatsbyImage } from 'gatsby-plugin-image';
+
+export default function BlogPost({ data }) {
+    const { html } = data.markdownRemark;
+    const { title, date, featuredImage, slug } = data.markdownRemark.frontmatter;
     return (
         <Layout>
             <section className="layout-content">
-                <h2>title</h2>
-                <h3>info</h3>
-                <div>{/* <GatsbyImage /> */}</div>
-                {/* <div dangerouslySetInnerHTML={} /> */}
+                <h2>{title}</h2>
+                <span>{date}</span>
+                <div>
+                    <GatsbyImage image={getImage(featuredImage)} alt={slug} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: html }} />
             </section>
         </Layout>
     );
 }
+
+export const query = graphql`
+    query BlogPost($slug: String) {
+        markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+            frontmatter {
+                slug
+                featuredImage {
+                    childImageSharp {
+                        gatsbyImageData(placeholder: BLURRED, width: 500)
+                    }
+                }
+                title
+                date
+            }
+            html
+        }
+    }
+`;
