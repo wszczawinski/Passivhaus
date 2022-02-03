93b556d9249432d71be2160980174428152ba3cf
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2022-02-03 10:57:13 +0000

    feat(footerStyling)

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import pibpLogo from '../../images/pibpLogo.png';
+
+import './Footer.scss';
 
 export default function Footer() {
     return (
-        <div>
-            <p>
-                Polski Instytut Budownictwa Pasywnego i Energii Odnawialnej Imienia Güntera
-                Schlagowskiego Sp. z o.o.
+        <div className="footer">
+            <p className="footer__name">
+                <img src={pibpLogo} alt="logo" />
+                <span>
+                    Polski Instytut Budownictwa Pasywnego i Energii Odnawialnej Imienia Güntera
+                    Schlagowskiego Sp. z o.o.
+                </span>
             </p>
+            <p className="footer__contact">
+                <span>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <a href="mailto:pibp@pibp.pl"> pibp@pibp.pl</a>
+                </span>
+                <span>
+                    <FontAwesomeIcon icon={faPhone} />
+                    <a href="tel:+48585241200"> (0048) 58 524 12 00</a>
+                </span>
 
-            <p>
-                ul. Homera 55 80-299 GDAŃSK (Osowa) <br />
+                <span>Fax: (0048) 58 522 98 50</span>
+            </p>
+            <p className="footer__adress">
+                ul. Homera 55, 80-299 Gdańsk (Osowa) <br />
                 NIP: 204-000-04-44 <br />
                 Instytut działa na zasadach non-profit
-            </p>
-            <p>
-                Telefon: (0048) 58 524 12 00 <br />
-                Fax: (0048) 58 522 98 50 <br />
-                E-mail: pibp@pibp.pl
             </p>
         </div>
     );
