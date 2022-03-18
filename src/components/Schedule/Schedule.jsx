e6f2e3a6122cab242a8fba1754d5ece943ee9a90
Kinga Zawarczynska <kinga.zawarczynska@justjoin.it>
2022-03-18 10:39:10 +0100

    feat(reklama): add advertise img

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import number1 from '../../images/home/1.svg';
-import number2 from '../../images/home/2.svg';
-import number3 from '../../images/home/3.svg';
+import reklama from '../../images/home/szkolenie_reklama_2.svg';
 
 import './Schedule.scss';
 
-export function Schedule({ dates, place, title }) {
+export function Schedule({ title }) {
     return (
         <div className="schedule">
             <span className="schedule__title">Terminarz najbliższych szkoleń:</span>
-            <span className="schedule__training-title">{title}</span>
-            <div className="schedule__date">
-                <img src={number1} alt="zjazd nr 1" />
-                {dates[0]}
-            </div>
-            <div className="schedule__date">
-                <img src={number2} alt="zjazd nr 2" />
-                {dates[1]}
-            </div>
-            <div className="schedule__date">
-                <img src={number3} alt="zjazd nr 3" />
-                {dates[2]}
-            </div>
-            <span>{place}</span>
+            <img src={reklama} alt="reklama_szkolenia" />
         </div>
     );
 }
